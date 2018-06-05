2008f362d4692816de92bbf98c563bf99ba8de1d
Aiden Loyd <aidensaloyd@gmail.com>
2018-06-05 11:00:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function total() {
 
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
-    if (item.itemName == cart[i].itemName) {
+    if (item.itemName === cart[i].itemName) {
       cart.splice(i, 1);
       return cart;
     }
